9c3894963fd11aba1cc5f2d910777d3b13b3e5c5
Clément Guérin <geecko.dev@free.fr>
2015-05-23 22:30:36 +0200

    remove navigation buttons in the menu, add back button

--- boards/dk-tm4c129x/upmc_raycast/menu.c
+++ boards/dk-tm4c129x/upmc_raycast/menu.c
@@ -13,8 +13,7 @@
 
 static void OnMenuPlayPress(tWidget *psWidget);
 static void OnMenuOptionsPress(tWidget *psWidget);
-static void OnPrevious(tWidget *psWidget);
-static void OnNext(tWidget *psWidget);
+static void OnOptionsRetourPress(tWidget *psWidget);
 static void OnMenuPaint(tWidget *psWidget, tContext *psContext);
 static void OnOptionsPaint(tWidget *psWidget, tContext *psContext);
 extern tCanvasWidget g_panels[];
@@ -25,17 +24,22 @@ Canvas(g_menu, g_panels, 0, 0, &g_sKentec320x240x16_SSD2119,
 RectangularButton(g_menuOptions, g_panels, &g_menu, 0,
                   &g_sKentec320x240x16_SSD2119, 320/2 - 100/2, 90, 100, 30,
                   PB_STYLE_FILL | PB_STYLE_OUTLINE | PB_STYLE_TEXT,
-                  ClrMidnightBlue, ClrBlack, ClrGray, ClrSilver,
+                  ClrMidnightBlue, ClrMidnightBlue, ClrGray, ClrSilver,
                   g_psFontCm22, "Options", 0, 0, 0, 0, OnMenuOptionsPress);
 RectangularButton(g_menuPlay, g_panels, &g_menuOptions, 0,
                   &g_sKentec320x240x16_SSD2119, 320/2 - 100/2, 60, 100, 30,
                   PB_STYLE_FILL | PB_STYLE_OUTLINE | PB_STYLE_TEXT,
-                  ClrMidnightBlue, ClrBlack, ClrGray, ClrSilver,
+                  ClrMidnightBlue, ClrMidnightBlue, ClrGray, ClrSilver,
                   g_psFontCm22, "Jouer", 0, 0, 0, 0, OnMenuPlayPress);
 
 Canvas(g_sOptions, g_panels + 1, 0, 0, &g_sKentec320x240x16_SSD2119,
        0, 0, 320, 160,
        CANVAS_STYLE_APP_DRAWN, 0, 0, 0, 0, 0, 0, OnOptionsPaint);
+RectangularButton(g_sOptionsBack, g_panels, &g_sOptions, 0,
+                  &g_sKentec320x240x16_SSD2119, 20, 20, 100, 30,
+                  PB_STYLE_FILL | PB_STYLE_OUTLINE | PB_STYLE_TEXT,
+                  ClrMidnightBlue, ClrMidnightBlue, ClrGray, ClrSilver,
+                  g_psFontCm22, "Retour", 0, 0, 0, 0, OnOptionsRetourPress);
 
 //*****************************************************************************
 //
@@ -48,43 +52,11 @@ tCanvasWidget g_panels[] =
     CanvasStruct(0, 0, &g_menuPlay, &g_sKentec320x240x16_SSD2119,
                  0, 0, 320, 160,
                  CANVAS_STYLE_FILL, ClrBlack, 0, 0, 0, 0, 0, 0),
-    CanvasStruct(0, 0, &g_sOptions, &g_sKentec320x240x16_SSD2119,
+    CanvasStruct(0, 0, &g_sOptionsBack, &g_sKentec320x240x16_SSD2119,
                  0, 0, 320, 160,
                  CANVAS_STYLE_FILL, ClrBlack, 0, 0, 0, 0, 0, 0),
 };
 #define NUM_PANELS (sizeof(g_panels) / sizeof(g_panels[0]))
-
-//*****************************************************************************
-//
-// The names for each of the panels, which is displayed at the bottom of the
-// screen.
-//
-//*****************************************************************************
-static char *g_pcPanelNames[] =
-{
-    "     Menu     ",
-    "     Options     ",
-};
-
-//*****************************************************************************
-//
-// The buttons and text across the bottom of the screen.
-//
-//*****************************************************************************
-RectangularButton(g_sPrevious, 0, 0, 0, &g_sKentec320x240x16_SSD2119,
-                  0, 182, 50, 50,
-                  PB_STYLE_FILL, ClrBlack, ClrBlack, 0, ClrSilver,
-                  g_psFontCm20, "-", 0, 0,
-                  0, 0, OnPrevious);
-Canvas(g_sTitle, 0, 0, 0, &g_sKentec320x240x16_SSD2119,
-       50, 182, 204, 50,
-       CANVAS_STYLE_TEXT | CANVAS_STYLE_TEXT_OPAQUE, 0, 0, ClrSilver,
-       g_psFontCm20, 0, 0, 0);
-RectangularButton(g_sNext, 0, 0, 0, &g_sKentec320x240x16_SSD2119,
-                  (320-50), 182, 50, 50,
-                  PB_STYLE_IMG | PB_STYLE_TEXT, ClrBlack, ClrBlack, 0,
-                  ClrSilver, g_psFontCm20, "+", 0,
-                  0, 0, 0, OnNext);
 
 //*****************************************************************************
 //
@@ -103,145 +75,19 @@ OnMenuPlayPress(tWidget *psWidget)
 static void
 OnMenuOptionsPress(tWidget *psWidget)
 {
-    OnNext(psWidget);
-}
-
-//*****************************************************************************
-//
-// Handles presses of the previous panel button.
-//
-//*****************************************************************************
-static void
-OnPrevious(tWidget *psWidget)
-{
-    //
-    // There is nothing to be done if the first panel is already being
-    // displayed.
-    //
-    if(g_ui32Panel == 0)
-    {
-        return;
-    }
-
-    //
-    // Remove the current panel.
-    //
     WidgetRemove((tWidget *)(g_panels + g_ui32Panel));
-
-    //
-    // Decrement the panel index.
-    //
-    g_ui32Panel--;
-
-    //
-    // Add and draw the new panel.
-    //
+    g_ui32Panel = 1;
     WidgetAdd(WIDGET_ROOT, (tWidget *)(g_panels + g_ui32Panel));
     WidgetPaint((tWidget *)(g_panels + g_ui32Panel));
-
-    //
-    // Set the title of this panel.
-    //
-    CanvasTextSet(&g_sTitle, g_pcPanelNames[g_ui32Panel]);
-    WidgetPaint((tWidget *)&g_sTitle);
-
-    //
-    // See if this is the first panel.
-    //
-    if(g_ui32Panel == 0)
-    {
-        //
-        // Clear the previous button from the display since the first panel is
-        // being displayed.
-        //
-        PushButtonImageOff(&g_sPrevious);
-        PushButtonTextOff(&g_sPrevious);
-        PushButtonFillOn(&g_sPrevious);
-        WidgetPaint((tWidget *)&g_sPrevious);
-    }
-
-    //
-    // See if the previous panel was the last panel.
-    //
-    if(g_ui32Panel == (NUM_PANELS - 2))
-    {
-        //
-        // Display the next button.
-        //
-        PushButtonImageOn(&g_sNext);
-        PushButtonTextOn(&g_sNext);
-        PushButtonFillOff(&g_sNext);
-        WidgetPaint((tWidget *)&g_sNext);
-    }
 }
 
-//*****************************************************************************
-//
-// Handles presses of the next panel button.
-//
-//*****************************************************************************
 static void
-OnNext(tWidget *psWidget)
+OnOptionsRetourPress(tWidget *psWidget)
 {
-    //
-    // There is nothing to be done if the last panel is already being
-    // displayed.
-    //
-    if(g_ui32Panel == (NUM_PANELS - 1))
-    {
-        return;
-    }
-
-    //
-    // Remove the current panel.
-    //
     WidgetRemove((tWidget *)(g_panels + g_ui32Panel));
-
-    //
-    // Increment the panel index.
-    //
-    g_ui32Panel++;
-
-    //
-    // Add and draw the new panel.
-    //
+    g_ui32Panel = 0;
     WidgetAdd(WIDGET_ROOT, (tWidget *)(g_panels + g_ui32Panel));
     WidgetPaint((tWidget *)(g_panels + g_ui32Panel));
-
-    //
-    // Set the title of this panel.
-    //
-    CanvasTextSet(&g_sTitle, g_pcPanelNames[g_ui32Panel]);
-    WidgetPaint((tWidget *)&g_sTitle);
-
-    //
-    // See if the previous panel was the first panel.
-    //
-    if(g_ui32Panel == 1)
-    {
-        //
-        // Display the previous button.
-        //
-        PushButtonImageOn(&g_sPrevious);
-        PushButtonTextOn(&g_sPrevious);
-        PushButtonFillOff(&g_sPrevious);
-        WidgetPaint((tWidget *)&g_sPrevious);
-    }
-
-    //
-    // See if this is the last panel.
-    //
-    if(g_ui32Panel == (NUM_PANELS - 1))
-    {
-        //
-        // Clear the next button from the display since the last panel is being
-        // displayed.
-        //
-        PushButtonImageOff(&g_sNext);
-        PushButtonTextOff(&g_sNext);
-        PushButtonFillOn(&g_sNext);
-        WidgetPaint((tWidget *)&g_sNext);
-    }
 }
 
 //*****************************************************************************
@@ -272,19 +118,10 @@ void
 menuInit()
 {
     //
-    // Add the title block and the previous and next buttons to the widget
-    // tree.
-    //
-    WidgetAdd(WIDGET_ROOT, (tWidget *)&g_sPrevious);
-    WidgetAdd(WIDGET_ROOT, (tWidget *)&g_sTitle);
-    WidgetAdd(WIDGET_ROOT, (tWidget *)&g_sNext);
-
-    //
     // Add the first panel to the widget tree.
     //
     g_ui32Panel = 0;
     WidgetAdd(WIDGET_ROOT, (tWidget *)g_panels);
-    CanvasTextSet(&g_sTitle, g_pcPanelNames[0]);
 
     //
     // Issue the initial paint request to the widgets.
